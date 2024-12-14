5ecdbac286a376424fdd3feddfec7df060fe3f54
Quang Pham <quangpn.developer@gmail.com>
2024-12-14 16:57:34 +0700

    Google services configuration (#3)
    * Update app themes
    
    * Google services configuration

--- src/configs/firebase-config.ts
+++ src/configs/firebase-config.ts
@@ -5,7 +5,7 @@ import { getFirestore } from 'firebase/firestore'
 const firebaseConfig = {
   apiKey: process.env.EXPO_PUBLIC_API_KEY,
   authDomain: process.env.EXPO_PUBLIC_AUTH_DOMAIN,
-  projectId: process.env.EXPO_PUBLIC_PROJECT_ID,
+  projectId: process.env.EXPO_PUBLIC_EAS_PROJECT_ID,
   storageBucket: process.env.EXPO_PUBLIC_STORAGE_BUCKET,
   messagingSenderId: process.env.EXPO_PUBLIC_MESSAGING_SENDER_ID,
   appId: process.env.EXPO_PUBLIC_APP_ID,
@@ -17,6 +17,4 @@ export const FIREBASE_AUTH = initializeAuth(FIREBASE_APP, {
   // TODO
   // persistence:
 })
-
-// Ottieni una connessione al database Firestore per la gestione dei dati
 export const FIREBASE_DB = getFirestore(FIREBASE_APP)
